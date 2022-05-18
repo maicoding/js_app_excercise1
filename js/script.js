5f3413b827e39d4636f4df1391224df76ec2d317
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-05-18 21:50:37 +0200

    Update script.js
    Define pokemonList Variable, add  arrays of pokemons with name, height and type

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-alert('Hello World');
-
-let favoriteFood = 'French Fries';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Ivysaur', height: '1.0', type: ['Grass', 'Poison']},
+  {name: 'Charmander', height: '0.6', type: 'Fire'},
+  {name: 'Arbok', height: '3.5', type: 'Poison'},
+  {name: 'Paras', height: '0.3', type: ['Grass', 'Bug']},
+  {name: 'Abra', height:'0.9', type: 'Psychic'},
+];
