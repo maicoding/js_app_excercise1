898e82aa185b3a6f1debae55db3acb1475e54afc
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-05-20 17:18:49 +0200

    Update script.js
    Use identation and spacing, use array for all objects.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-let pokemonList = [
-  {name: 'Ivysaur', height: '1.0', type: ['Grass', 'Poison']},
-  {name: 'Charmander', height: '0.6', type: 'Fire'},
-  {name: 'Arbok', height: '3.5', type: 'Poison'},
-  {name: 'Paras', height: '0.3', type: ['Grass', 'Bug']},
-  {name: 'Abra', height:'0.9', type: 'Psychic'},
-];
+let pokemonList = [{
+	name: 'Ivysaur',
+	height: '1.0',
+	type: ['Grass', 'Poison']
+}, {
+	name: 'Charmander',
+	height: '0.6',
+	type: ['Fire']
+}, {
+	name: 'Arbok',
+	height: '3.5',
+	type: ['Poison']
+}, {
+	name: 'Paras',
+	height: '0.3',
+	type: ['Grass', 'Bug']
+}, {
+	name: 'Abra',
+	height: '0.9',
+	type: ['Psychic']
+}, ];
