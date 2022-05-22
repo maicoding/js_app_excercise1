a1ffd921a9a5d729bd8ba3db3f999fd48df99866
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-05-22 12:32:01 +0200

    Update script.js
    add document write for index.htm

--- js/script.js
+++ js/script.js
@@ -19,3 +19,17 @@ let pokemonList = [{
 	height: 0.9,
 	type: ['Psychic']
 }, ];
+
+//Loop for displaying pokemons
+for (let i=0; i<pokemonList.length; i++){
+    document.write(pokemonList[i].name +` `+`height is `+ pokemonList[i].height +` m!`);
+
+//statement about height
+    if(pokemonList[i].height >= 1.5) {
+        document.write(`     - I\`m size L. Wow, that\`s big!`+ `<br>`);
+    } else if(pokemonList[i].height >=0.5 && pokemonList[i].height <=1.4) {
+        document.write(`     - I\`m size M. `+ `<br>`);
+    } else {
+        document.write(`     - I\`m size S. ` + `<br>`);
+    }
+}
