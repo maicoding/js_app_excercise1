7dd700cad78b1a6852b6ee05e6509ae3a0b4e4e3
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-05-22 13:12:14 +0200

    Update script.js
    add abilities

--- js/script.js
+++ js/script.js
@@ -1,41 +1,45 @@
 let pokemonList = [{
 	name: 'Ivysaur',
 	height: 1.0,
-	types: ['Grass', 'Poison']
+	types: ['Grass', 'Poison'],
+	abilities: ['Overgrow']
 }, {
 	name: 'Charmander',
 	height: 0.6,
-	types: ['Fire']
+	types: ['Fire'],
+	abilities: ['Blaze']
 }, {
 	name: 'Arbok',
 	height: 3.5,
-	types: ['Poison']
+	types: ['Poison'],
+	abilities: ['Shed Skin', 'Intimidate']
 }, {
 	name: 'Paras',
 	height: 0.3,
-	types: ['Grass', 'Bug']
+	types: ['Grass', 'Bug'],
+	abilities: ['Effect Spore', 'Dry Skin']
 }, {
 	name: 'Abra',
 	height: 0.9,
-	types: ['Psychic']
-},
-{
+	types: ['Psychic'],
+	abilities: ['Inner Focus', 'Synchronize']
+}, {
 	name: 'Wartortle',
 	height: 3.03,
-	types: ['Water']
-},
- ];
+	types: ['Water'],
+	abilities: ['Torrent']
+}, ];
 
 //Loop for displaying pokemons
-for (let i=0; i<pokemonList.length; i++){
-    document.write(pokemonList[i].name +' '+'height is '+ pokemonList[i].height +' m!');
+for (let i = 0; i < pokemonList.length; i++) {
+	document.write(pokemonList[i].name + ' ' + 'height is ' + pokemonList[i].height + ' m!');
 
-//statement about height
-    if(pokemonList[i].height >= 1.5) {
-        document.write('     - I\'m size L. Wow, that\'s big!'+ '<br>');
-    } else if(pokemonList[i].height >=0.5 && pokemonList[i].height <=1.4) {
-        document.write('     - I\'m size M. '+ '<br>');
-    } else {
-        document.write('     - I\'m size S. That\'s rather small! But do not underestimate me!' + '<br>');
-    }
+	//statement about height
+	if (pokemonList[i].height >= 1.5) {
+		document.write('     - I\'m size L. Wow, that\'s big!' + '<br>');
+	} else if (pokemonList[i].height >= 0.5 && pokemonList[i].height <= 1.4) {
+		document.write('     - I\'m size M. ' + '<br>');
+	} else {
+		document.write('     - I\'m size S. That\'s rather small! But do not underestimate me!' + '<br>');
+	}
 }
