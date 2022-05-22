be093126b334bd7bd12a23cb570f286891b5c1dc
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-05-22 10:47:07 +0200

    Update script.js
    change height variable to numbers

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 let pokemonList = [{
 	name: 'Ivysaur',
-	height: '1.0',
+	height: 1.0,
 	type: ['Grass', 'Poison']
 }, {
 	name: 'Charmander',
-	height: '0.6',
+	height: 0.6,
 	type: ['Fire']
 }, {
 	name: 'Arbok',
-	height: '3.5',
+	height: 3.5,
 	type: ['Poison']
 }, {
 	name: 'Paras',
-	height: '0.3',
+	height: 0.3,
 	type: ['Grass', 'Bug']
 }, {
 	name: 'Abra',
-	height: '0.9',
+	height: 0.9,
 	type: ['Psychic']
 }, ];
