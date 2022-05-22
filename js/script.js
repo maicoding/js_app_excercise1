c19eef7e82bed55d5d1872152824b0025c747f58
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-05-22 12:54:22 +0200

    Update script.js
    Change string of small pokemons.

--- js/script.js
+++ js/script.js
@@ -22,14 +22,14 @@ let pokemonList = [{
 
 //Loop for displaying pokemons
 for (let i=0; i<pokemonList.length; i++){
-    document.write(pokemonList[i].name +` `+`height is `+ pokemonList[i].height +` m!`);
+    document.write(pokemonList[i].name +' '+'height is '+ pokemonList[i].height +' m!');
 
 //statement about height
     if(pokemonList[i].height >= 1.5) {
-        document.write(`     - I\`m size L. Wow, that\`s big!`+ `<br>`);
+        document.write('     - I\'m size L. Wow, that\'s big!'+ '<br>');
     } else if(pokemonList[i].height >=0.5 && pokemonList[i].height <=1.4) {
-        document.write(`     - I\`m size M. `+ `<br>`);
+        document.write('     - I\'m size M. '+ '<br>');
     } else {
-        document.write(`     - I\`m size S. ` + `<br>`);
+        document.write('     - I\'m size S. That\'s rather small! But do not underestimate me!' + '<br>');
     }
 }
