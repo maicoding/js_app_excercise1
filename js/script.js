e758beeacf2a6befc0644168234b73184a8dfe67
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-05-25 22:00:35 +0200

    Update script.js
    Add unordered list.

--- js/script.js
+++ js/script.js
@@ -1,45 +1,54 @@
 let pokemonList = [{
-	name: 'Ivysaur',
-	height: 1.0,
-	types: ['Grass', 'Poison'],
-	abilities: ['Overgrow']
+    name: 'Ivysaur',
+    height: 1.0,
+    types: ['Grass', 'Poison'],
+    abilities: ['Overgrow']
 }, {
-	name: 'Charmander',
-	height: 0.6,
-	types: ['Fire'],
-	abilities: ['Blaze']
+    name: 'Charmander',
+    height: 0.6,
+    types: ['Fire'],
+    abilities: ['Blaze']
 }, {
-	name: 'Arbok',
-	height: 3.5,
-	types: ['Poison'],
-	abilities: ['Shed Skin', 'Intimidate']
+    name: 'Arbok',
+    height: 3.5,
+    types: ['Poison'],
+    abilities: ['Shed Skin', 'Intimidate']
 }, {
-	name: 'Paras',
-	height: 0.3,
-	types: ['Grass', 'Bug'],
-	abilities: ['Effect Spore', 'Dry Skin']
+    name: 'Paras',
+    height: 0.3,
+    types: ['Grass', 'Bug'],
+    abilities: ['Effect Spore', 'Dry Skin']
 }, {
-	name: 'Abra',
-	height: 0.9,
-	types: ['Psychic'],
-	abilities: ['Inner Focus', 'Synchronize']
+    name: 'Abra',
+    height: 0.9,
+    types: ['Psychic'],
+    abilities: ['Inner Focus', 'Synchronize']
 }, {
-	name: 'Wartortle',
-	height: 3.03,
-	types: ['Water'],
-	abilities: ['Torrent']
+    name: 'Wartortle',
+    height: 3.03,
+    types: ['Water'],
+    abilities: ['Torrent']
 }, ];
 
-//Loop for displaying pokemons
+// Display the data on the page as an unordered list
+document.write('<ul class="pokemon-list">');
+// for loop
 for (let i = 0; i < pokemonList.length; i++) {
-	document.write(pokemonList[i].name + ' ' + 'height is ' + pokemonList[i].height + ' m! ');
+    let currentPokemon = pokemonList[i];
 
-	//statement about height
-	if (pokemonList[i].height >= 1.5) {
-		document.write(pokemonList[i].name + ' is size L. Wow, that\'s big!' + '<br>');
-	} else if (pokemonList[i].height >= 0.5 && pokemonList[i].height <= 1.4) {
-		document.write(pokemonList[i].name + ' is size M. ' + '<br>');
-	} else {
-		document.write(pokemonList[i].name + ' is size S. That\'s rather small! But do not underestimate me!' + '<br>');
-	}
+    // opening list element tag
+    document.write(`
+    <li class="pokemon-list__item">
+      ${currentPokemon.name} (height: <span>${currentPokemon.height}</span>)
+  `);
+    if (currentPokemon.height > 1.4) {
+        document.write(' - Wow, that\'s big!');
+    }
+    if (currentPokemon.height < 1.4) {
+        document.write(' - That\'s quite small!');
+    }
+    // closing the list element tag
+    document.write('</li>');
 }
+// closing the ul tag
+document.write('</ul>');
