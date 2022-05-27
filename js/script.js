c4ed9972656574fb0bf531979bb67c750882c99e
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-05-27 20:51:49 +0200

    IIFE and For each Loop and add
    add IIFE and a for each loop and the add function.

--- js/script.js
+++ js/script.js
@@ -1,54 +1,69 @@
-let pokemonList = [{
-    name: 'Ivysaur',
-    height: 1.0,
-    types: ['Grass', 'Poison'],
-    abilities: ['Overgrow']
-}, {
-    name: 'Charmander',
-    height: 0.6,
-    types: ['Fire'],
-    abilities: ['Blaze']
-}, {
-    name: 'Arbok',
-    height: 3.5,
-    types: ['Poison'],
-    abilities: ['Shed Skin', 'Intimidate']
-}, {
-    name: 'Paras',
-    height: 0.3,
-    types: ['Grass', 'Bug'],
-    abilities: ['Effect Spore', 'Dry Skin']
-}, {
-    name: 'Abra',
-    height: 0.9,
-    types: ['Psychic'],
-    abilities: ['Inner Focus', 'Synchronize']
-}, {
-    name: 'Wartortle',
-    height: 3.03,
-    types: ['Water'],
-    abilities: ['Torrent']
-}, ];
+// IIFE
+let pokemonRepository = (function () {
+    let pokemonList = [{
+        name: 'Ivysaur',
+        height: 1.0,
+        types: ['Grass', 'Poison'],
+        abilities: ['Overgrow']
+    }, {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['Fire'],
+        abilities: ['Blaze']
+    }, {
+        name: 'Arbok',
+        height: 3.5,
+        types: ['Poison'],
+        abilities: ['Shed Skin', 'Intimidate']
+    }, {
+        name: 'Paras',
+        height: 0.3,
+        types: ['Grass', 'Bug'],
+        abilities: ['Effect Spore', 'Dry Skin']
+    }, {
+        name: 'Abra',
+        height: 0.9,
+        types: ['Psychic'],
+        abilities: ['Inner Focus', 'Synchronize']
+    },]
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+pokemonRepository.add({
+    name: 'Wartortle', 'height': 3.03, types: ['Water'], abilities: ['Torrent']
+});
 
 // Display the data on the page as an unordered list
 document.write('<ul class="pokemon-list">');
-// for loop
-for (let i = 0; i < pokemonList.length; i++) {
-    let currentPokemon = pokemonList[i];
+let pokemonList = pokemonRepository.getAll();
+// for each
+pokemonList.forEach(function (pokemon) {
 
     // opening list element tag
     document.write(`
     <li class="pokemon-list__item">
-      ${currentPokemon.name} is <span>${currentPokemon.height}</span> m.
+      ${pokemon.name} is <span>${pokemon.height}</span> m.
   `);
-    if (currentPokemon.height > 1.9) {
+    if (pokemon.height > 1.9) {
         document.write(' - Wow, that\'s big!');
     }
-    if (currentPokemon.height < 0.8) {
+    if (pokemon.height < 0.8) {
         document.write(' - That\'s quite small!');
     }
     // closing the list element tag
     document.write('</li>');
 }
+);
 // closing the ul tag
 document.write('</ul>');
