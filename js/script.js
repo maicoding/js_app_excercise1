1ed42cb556b9549a3a4fbc3b6080285e6a3e7577
Claudia Mai <50277594+maicoding@users.noreply.github.com>
2022-05-28 11:29:54 +0200

    Update script.js
    change formatting

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
-<<<<<<< Updated upstream
 // IIFE
-=======
-let pokemonList = [{
-    name: 'Ivysaur',
-    height: 1.0,
-    types: ['Grass', 'Poison'],
-    abilities: ['Overgrow']
-}, {
-    name: 'Charmander',
-    height: 0.6,
-    types: ['Fire'],
-    abilities: ['Blaze']
-}, {// IIFE
->>>>>>> Stashed changes
 let pokemonRepository = (function () {
     let pokemonList = [{
         name: 'Ivysaur',
@@ -40,6 +26,7 @@ let pokemonRepository = (function () {
         types: ['Psychic'],
         abilities: ['Inner Focus', 'Synchronize']
     },]
+
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
@@ -47,71 +34,22 @@ let pokemonRepository = (function () {
     function getAll() {
         return pokemonList;
     }
-
     return {
         add: add,
         getAll: getAll
     };
 })();
-
 pokemonRepository.add({
-    name: 'Wartortle', 'height': 3.03, types: ['Water'], abilities: ['Torrent']
-});
-<<<<<<< Updated upstream
-=======
-
-// Display the data on the page as an unordered list
-document.write('<ul class="pokemon-list">');
-let pokemonList = pokemonRepository.getAll();
-// for each
-pokemonList.forEach(function (pokemon) {
-
-    // opening list element tag
-    document.write(`
-    <li class="pokemon-list__item">
-      ${pokemon.name} is <span>${pokemon.height}</span> m.
-  `);
-    if (pokemon.height > 1.9) {
-        document.write(' - Wow, that\'s big!');
-    }
-    if (pokemon.height < 0.8) {
-        document.write(' - That\'s quite small!');
-    }
-    // closing the list element tag
-    document.write('</li>');
-}
-);
-// closing the ul tag
-document.write('</ul>');
-
-    name: 'Arbok',
-    height: 3.5,
-    types: ['Poison'],
-    abilities: ['Shed Skin', 'Intimidate']
-}, {
-    name: 'Paras',
-    height: 0.3,
-    types: ['Grass', 'Bug'],
-    abilities: ['Effect Spore', 'Dry Skin']
-}, {
-    name: 'Abra',
-    height: 0.9,
-    types: ['Psychic'],
-    abilities: ['Inner Focus', 'Synchronize']
-}, {
     name: 'Wartortle',
-    height: 3.03,
+    'height': 3.03,
     types: ['Water'],
     abilities: ['Torrent']
-}, ];
->>>>>>> Stashed changes
-
+});
 // Display the data on the page as an unordered list
 document.write('<ul class="pokemon-list">');
 let pokemonList = pokemonRepository.getAll();
 // for each
 pokemonList.forEach(function (pokemon) {
-
     // opening list element tag
     document.write(`
     <li class="pokemon-list__item">
@@ -125,7 +63,6 @@ pokemonList.forEach(function (pokemon) {
     }
     // closing the list element tag
     document.write('</li>');
-}
-);
+});
 // closing the ul tag
 document.write('</ul>');
